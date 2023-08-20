a907648060820bcd2d5c2e9c9ebd89693500576b
ali abdelnaby <aliabdelnaby2392002@gmail.com>
2023-08-21 01:09:14 +0200

    change addnote loading

--- lib/views/widgets/custom_button.dart
+++ lib/views/widgets/custom_button.dart
@@ -1,12 +1,12 @@
-
 import 'package:flutter/material.dart';
 
 import '../../constants.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key, this.onTap});
+  const CustomButton({super.key, this.onTap, this.isLoading = false});
 
-final void Function()? onTap;
+  final void Function()? onTap;
+  final bool isLoading;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -18,15 +18,23 @@ final void Function()? onTap;
           color: kPrimaryColor,
           borderRadius: BorderRadius.circular(8),
         ),
-        child: const Center(
-          child: Text(
-            'Add',
-            style: TextStyle(
-              color: Colors.black,
-               fontSize: 18,
-               fontWeight: FontWeight.w500,
-               ),
-          ),
+        child: Center(
+          child: isLoading
+              ? const SizedBox(
+                height: 24,
+                width: 24,
+                child: CircularProgressIndicator(
+                  color: Colors.black,
+                ),
+              )
+              : const Text(
+                  'Add',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
         ),
       ),
     );
